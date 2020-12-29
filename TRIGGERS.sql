d6d8fc0699131333c4c5ba3303d272e3a44ffdb7
avacabillan <73097784+avacabillan@users.noreply.github.com>
2020-12-29 20:08:08 +0800

    Update TRIGGERS.sql

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -1,2 +1,5 @@
+ALTER TABLE customers ADD COLUMN complete_address VARCHAR(50);
+UPDATE customers SET complete_address = CONCAT(street,', ',city,', ',state,' ',zip_code);
+
 CREATE TRIGGER insert_trigger BEFORE INSERT ON customers FOR EACH ROW SET new.complete_address = CONCAT(new.street,', ', new.city,', ',new.state,' ',new.zip_code);
 CREATE TRIGGER update_trigger BEFORE UPDATE ON customers FOR EACH ROW SET new.complete_address = CONCAT(new.street,', ', new.city,', ',new.state,' ',new.zip_code);
